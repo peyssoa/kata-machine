e81319c401f5f8d64b7fe54182ae9774dfe157cd
Christiane Celeste <peyssoa@gmail.com>
2024-12-09 09:21:24 -0400

    Binary Tree traversal

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,21 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    //  Find the base path
+    if (!curr) {
+        return path;
+    }
 
+    // recurse
+    // pre
+
+    // recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+    path.push(curr.value);
+
+    // post
+    return path;
+}
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
